200af6e14e488981b02fe79e6e2f1a228f789dce
gungun1010 <gungun1010@Leons-Air-3.wireless.yale.internal>
2015-03-27 00:25:11 -0400

    Revert 61db5b8..16d62b6
    This rolls back to commit 61db5b8b74dce8a71d4b76a196b3b922e1f57625.

--- HW2/problem2.m
+++ HW2/problem2.m
@@ -13,19 +13,6 @@ label = out{3};
 
 X = [feature1, feature2];
 Y = zeros(size(label,1),1);
-
-%open the testing dataset
-fid = fopen('dataset2/dataset2.2.test.csv');
-out = textscan(fid,'%f%f%s','delimiter',',');
-fclose(fid);
-
-feature1_test = out{1};
-feature2_test = out{2};
-label_test = out{3};
-
-X_test = [feature1_test, feature2_test];
-Y_test = zeros(size(label_test,1),1);
-
 for i=1:size(label,1)
     if strcmp(label(i,1),'True') == 1
         Y(i,1) = 1;
@@ -33,22 +20,14 @@ for i=1:size(label,1)
         Y(i,1) = -1;
     end
 end
-
-for i=1:size(label_test,1)
-    if strcmp(label_test(i,1),'True') == 1
-        Y_test(i,1) = 1;
-    else
-        Y_test(i,1) = -1;
-    end
-end
-
+        
 figure;
-gscatter(X_test(:,1),X_test(:,2),Y_test);
+gscatter(X(:,1),X(:,2),Y);
 title('Scatter Diagram of Data');
 
 fprintf('start training\n');
 
-sigma = 0.01;
+sigma = 1;
 gamma = 1/(2*sigma^2);
 
 % Libsvm options
