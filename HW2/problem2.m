d37ce2df00ec3e18940eb37aab1d9447331ffa62
gungun1010 <gungun1010@Leons-MacBook-Air-3.local>
2015-03-27 03:12:50 -0400

    2.3

--- HW2/problem2.m
+++ HW2/problem2.m
@@ -20,23 +20,56 @@ for i=1:size(label,1)
         Y(i,1) = -1;
     end
 end
-        
+
+fid = fopen('dataset2/dataset2.2.test.csv');
+out = textscan(fid,'%f%f%s','delimiter',',');
+fclose(fid);
+
+feature1_test = out{1};
+feature2_test = out{2};
+label_test = out{3};
+
+X_test = [feature1, feature2];
+Y_test = zeros(size(label_test,1),1);
+for i=1:size(label_test,1)
+    if strcmp(label_test(i,1),'True') == 1
+        Y_test(i,1) = 1;
+    else
+        Y_test(i,1) = -1;
+    end
+end
+
 figure;
-gscatter(X(:,1),X(:,2),Y);
+gscatter(X_test(:,1),X_test(:,2),Y);
 title('Scatter Diagram of Data');
 
 fprintf('start training\n');
 
-sigma = 0.1;
-gamma = 1/(2*sigma^2);
+sigmaArr = 1:-0.02:0.01;
+testErr = zeros(size(sigmaArr,1),1);
+trainErr = zeros(size(sigmaArr,1),1);
+i=1;
+for sigma = 1:-0.02:0.01
+    gamma = 1/(2*sigma^2);
 
-% Libsvm options
-% -s 0 : classification
-% -t 2 : RBF kernel
-% -g : gamma in the RBF kernel
-model = svmtrain(Y, X, sprintf('-s 0 -t 2 -g %g', gamma));
+    % Libsvm options
+    % -s 0 : classification
+    % -t 2 : RBF kernel
+    % -g : gamma in the RBF kernel
+    model = svmtrain(Y, X, sprintf('-s 0 -t 2 -g %g', gamma));
 
-[predicted_label, accuracy, decision_values] = svmpredict(Y, X, model);
+    [predicted_label, accuracy, decision_values] = svmpredict(Y, X, model);
+    [predicted_label_test, accuracy_test, decision_values_test] = svmpredict(Y_test, X_test, model);
+    testErr(i,1)= accuracy_test(1,1);
+    trainErr(i,1)= accuracy(1,1);
+    i=i+1;
+    
+    display(sigma);
+end
+figure
+plot(1./sigmaArr,testErr,1./sigmaArr,trainErr);
+xlabel('1/\\sigma');
+ylabel('accuracy');
 
 % Plot heatmap
 plotboundary(Y, X, model, 't');
